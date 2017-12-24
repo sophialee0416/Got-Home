bebd49934d1d52a965ba4c8489e0f33dbbc72703
Usman Farooqi <uhfarooqi@gmail.com>
2017-12-23 22:21:18 -0600

    fixed security issue

--- app/src/main/java/com/example/gh/gothomeapp/home.java
+++ app/src/main/java/com/example/gh/gothomeapp/home.java
@@ -4,22 +4,55 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.view.ViewGroup;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
-public class home extends Fragment {
+public class home extends Fragment implements View.OnClickListener{
+    Button button;
     public static home newInstance() {
         home fragment = new home();
         return fragment;
     }
+    public static final String ACCOUNT_SID = ""; //Saved Keys to my notes
+    public static final String AUTH_TOKEN = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
+    /*public void buttonTest(View v){
+        Button button=(Button) v;
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        Message message = Message.creator(new PhoneNumber("+18329561743"),
+                new PhoneNumber("+18327667179"), "Hey your daughter is home!").create();
+
+        System.out.println(message.getSid());
+        ((Button) v).setText("clicked");
+    }*/
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(this);
+        return view;
     }
+
+    @Override
+    public void onClick(View v){
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(new PhoneNumber("+18329561743"),
+                new PhoneNumber("+18327667179"), "Hey your daughter is home!").create();
+    }
+
+
 }
